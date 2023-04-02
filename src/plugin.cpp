b2772bc71f8221d8a0921f92b42f093b73c89347
engsr6982 <pwxg286704@163.com>
2023-04-02 20:55:41 +0800

    瞎改

--- src/plugin.cpp
+++ src/plugin.cpp
@@ -1,21 +1,30 @@
-/**
- * @file plugin.cpp
- * @brief The main file of the plugin
- */
+﻿#include <string>
+#include <iostream>
 
-#include <llapi/LoggerAPI.h>
+#include <llapi/mc/CommandOrigin.hpp>
+#include <llapi/mc/CommandOutput.hpp>
+#include <llapi/mc/ItemStack.hpp>
+#include <llapi/mc/Level.hpp>
+#include <llapi/mc/Player.hpp>
+#include <llapi/mc/Types.hpp>
 
-#include "version.h"
+#include <llapi/DynamicCommandAPI.h>
+#include <llapi/EventAPI.h>
+#include <llapi/GlobalServiceAPI.h>
 
-// We recommend using the global logger.
 extern Logger logger;
 
-/**
- * @brief The entrypoint of the plugin. DO NOT remove or rename this function.
- *        
- */
 void PluginInit()
 {
-    // Your code here
-    logger.info("Hello, world!");
+    if (!std::filesystem::exists("./plugins/EULA"))
+    {
+        std::string str;//声明一个str的字符串变量
+        std::cout << "请同意MoJang　EULA\n";//显示初始内容
+        std::getline(std::cin, str);  // 从控制台输入一行字符串
+        if (str != "y" && str != "yes")
+        {
+            exit(0);
+        }
+        WriteAllFile("./plugins/EULA", "true");
+    }
 }
